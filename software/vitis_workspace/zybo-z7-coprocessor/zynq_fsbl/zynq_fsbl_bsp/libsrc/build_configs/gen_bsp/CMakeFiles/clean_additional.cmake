792b20ec80b16d4bf50291ac061f9fa30ca380c5
Joseph Abbey <me@josephabbey.dev>
2024-11-08 11:14:48 +0000

    Initial Commit

---
+++ software/vitis_workspace/zybo-z7-coprocessor/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,19 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\diskio.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ff.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ffconf.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\sleep.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs_config.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilrsa.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xiltimer.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xtimer_config.h"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilffs.a"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilrsa.a"
+  "D:\\repos\\zybo-z7-coprocessor\\software\\vitis_workspace\\zybo-z7-coprocessor\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxiltimer.a"
+  )
+endif()
